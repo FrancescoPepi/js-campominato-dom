de52ffe8b8d5d436c7d9b6ec3d4250cb014abb7c
FrancescoPepi <francescopepi16@gmail.com>
2023-08-22 17:04:25 +0200

    add random system

--- JS/main.js
+++ JS/main.js
@@ -6,6 +6,7 @@ const difficulty = document.getElementById("difficulty");
 // button start
 playButton.addEventListener("click", function () {
   let cellsTotal = parseInt(difficulty.value);
+
   gridGenerate(cellsTotal, cellContainer);
 });
 
@@ -14,10 +15,15 @@ playButton.addEventListener("click", function () {
  * @param {int} cellsTotal quantità di celle da creare
  * @param {tagContainer} cellsContainer DOC Contenitore per i cuby
  */
-function gridGenerate(cellsTotal, cellsContainer) {
-  cellsContainer.innerHTML = "";
+function gridGenerate(cellsTotal, cellContainer) {
+  cellContainer.innerHTML = "";
+  const whiteList = generateArray(1, cellsTotal, 1);
   for (let i = 1; i <= cellsTotal; i++) {
-    cellCreation(cellsTotal, i);
+    const randomIndex = randomNumber(0, whiteList.length - 1);
+    const cellValue = whiteList[randomIndex];
+    cellCreation(cellsTotal, cellValue);
+    whiteList.splice(randomIndex, 1);
+    console.log(whiteList + " poi random " + randomIndex);
   }
 }
 /**
@@ -25,10 +31,9 @@ function gridGenerate(cellsTotal, cellsContainer) {
  * @param {int} cellsTotal serve per fornire la grandezza del cubo
  * @param {int} i serve per dare un index ad ogni cella
  */
-function cellCreation(cellsTotal, i) {
-  //   const randomNumber = Math.floor(Math.random() * 100);
+function cellCreation(cellsTotal, cellValue) {
   const cell = document.createElement("li");
-  cell.setAttribute("data-index", i);
+  cell.setAttribute("data-index", cellValue);
   cell.classList.add("cell");
   cell.classList.add("lg-" + cellsTotal);
   cell.addEventListener("click", function () {
@@ -45,3 +50,14 @@ function cellCreation(cellsTotal, i) {
   //   cell.innerText = randomNumber;
   cellContainer.appendChild(cell);
 }
+
+function generateArray(from, to, step) {
+  const whiteList = [];
+  for (let i = from; i <= to; i += step) {
+    whiteList.push(i);
+  }
+  return whiteList;
+}
+
+const randomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
